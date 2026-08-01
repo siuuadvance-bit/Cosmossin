b7d164c7f0579a6d28b4f8146650527e9d7d5e99
Leonardo Rezende <tofupanquecacommel@gmail.com>
2026-08-01 18:52:43 -0400

    Clean & Fix: Limpeza de cache do Gradle + Imersão completa na Splash e App

--- android/app/src/main/java/com/cosmossin/app/MainActivity.java
+++ android/app/src/main/java/com/cosmossin/app/MainActivity.java
@@ -1,9 +1,6 @@
 package com.cosmossin.app;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
@@ -14,10 +11,7 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Desativa qualquer reserva de espaço do sistema
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        
-        // Bloqueio via API moderna
         WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
         controller.hide(WindowInsetsCompat.Type.systemBars());
         controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
@@ -27,7 +21,6 @@ public class MainActivity extends BridgeActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            // Re-força o bloqueio caso o usuário toque na tela ou saia e volte pro app
             WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
             controller.hide(WindowInsetsCompat.Type.systemBars());
         }
